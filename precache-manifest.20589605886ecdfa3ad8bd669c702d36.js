9e343a3ffac26783961ae2631b5192c8768d0311
Mateusz <matteuszd@gmail.com>
2019-03-29 19:07:49 +0100

    Updates

---
+++ precache-manifest.20589605886ecdfa3ad8bd669c702d36.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cba6890da728603c3c317dd6ce8f4e0",
+    "url": "/react-portfolio/static/media/nexter.1cba6890.png"
+  },
+  {
+    "revision": "4f0ae995bfd350e8d4c4",
+    "url": "/react-portfolio/static/css/main.b92565fe.chunk.css"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "89d03ef461cb947916e4",
+    "url": "/react-portfolio/static/js/2.aa7b7dae.chunk.js"
+  },
+  {
+    "revision": "1e9ef656a6bb00dcd81f0d706abc86ea",
+    "url": "/react-portfolio/static/media/sprite.1e9ef656.svg"
+  },
+  {
+    "revision": "4be3c74580ac6377fcb79083a3e2111e",
+    "url": "/react-portfolio/static/media/hero-tab.4be3c745.jpg"
+  },
+  {
+    "revision": "8fb79ac74abf92e898a36352949b670d",
+    "url": "/react-portfolio/static/media/hero-desktop.8fb79ac7.jpg"
+  },
+  {
+    "revision": "b47bfed2dbcd8c1e83cbbb7880a76518",
+    "url": "/react-portfolio/static/media/hero-phone.b47bfed2.jpg"
+  },
+  {
+    "revision": "62cbecefb6a9bb65e5584b04603414d5",
+    "url": "/react-portfolio/static/media/logo-white.62cbecef.svg"
+  },
+  {
+    "revision": "9ddb9757ac60ab70e9d41bc408ae17ae",
+    "url": "/react-portfolio/static/media/logo-white-small.9ddb9757.svg"
+  },
+  {
+    "revision": "9b830086bf7660ce1c47174267b4c3be",
+    "url": "/react-portfolio/static/media/natours.9b830086.png"
+  },
+  {
+    "revision": "c40b0ee82684dce3fd1a6227fd93a745",
+    "url": "/react-portfolio/static/media/trillo.c40b0ee8.png"
+  },
+  {
+    "revision": "4f0ae995bfd350e8d4c4",
+    "url": "/react-portfolio/static/js/main.5ac4e43f.chunk.js"
+  },
+  {
+    "revision": "c77deb8415c4388000b507afe27a922f",
+    "url": "/react-portfolio/static/media/react-weather-app.c77deb84.png"
+  },
+  {
+    "revision": "e342c9bf24b7ed58274bad4511e8ce08",
+    "url": "/react-portfolio/static/media/react-todo-app.e342c9bf.png"
+  },
+  {
+    "revision": "89b76d6f553de594c9a24a5908363a90",
+    "url": "/react-portfolio/static/media/milk.89b76d6f.png"
+  },
+  {
+    "revision": "f6962b1a095bc33728d2ec203f39f234",
+    "url": "/react-portfolio/static/media/proktolog.f6962b1a.png"
+  },
+  {
+    "revision": "23f6e4871a03799d774082913ed52cff",
+    "url": "/react-portfolio/static/media/old-portfolio.23f6e487.png"
+  },
+  {
+    "revision": "af5fa295269d106107d170d79bff31ef",
+    "url": "/react-portfolio/static/media/spanish-robot.af5fa295.png"
+  },
+  {
+    "revision": "f2020dbdd93a8f14f79ddaf5cd2b60b6",
+    "url": "/react-portfolio/static/media/Videalty.f2020dbd.png"
+  },
+  {
+    "revision": "70421dfc05564aacd318b853555c10c5",
+    "url": "/react-portfolio/static/media/NoweCV-final.70421dfc.pdf"
+  },
+  {
+    "revision": "622c03a740ec23c401b0f9bb846fb349",
+    "url": "/react-portfolio/static/media/NewCV-final.622c03a7.pdf"
+  },
+  {
+    "revision": "0f97c82ab66df7ae6f1aa65b33ec5644",
+    "url": "/react-portfolio/static/media/face.0f97c82a.jpg"
+  },
+  {
+    "revision": "1c070f2fce98dbce25673dad8320247d",
+    "url": "/react-portfolio/index.html"
+  }
+];
